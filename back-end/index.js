98b6dc308845589fd0da0a080e5fd0a8c74a7032
Pedro Campagnoli <pedrocampagnoli28@gmail.com>
2024-06-12 23:36:40 -0300

    ajustes no express

--- back-end/index.js
+++ back-end/index.js
@@ -1,25 +1,58 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const bcrypt = require('bcrypt');
-const User = require('./src/models/userModel');
-const hashPassword = require('./src/middlewares/hashPassword');
-const { generateToken, authenticateToken } = require('./src/middlewares/authService');
+const bcrypt = require("bcrypt");
+const User = require("./src/models/userModel");
+const hashPassword = require("./src/middlewares/hashPassword");
+const {
+  generateToken,
+  authenticateToken,
+} = require("./src/middlewares/authService");
 const port = 3000;
-const db = require('./src/config/database');
-var cors = require('cors')
+const db = require("./src/config/database");
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
-app.use(cors())
+// app.use(cors());
 db.sync();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-  app.post('/register', hashPassword, async (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/register", hashPassword, async (req, res) => {
+  try {
     const user = await User.create({ ...req.body });
     res.send(user);
-  });
-app.listen(port, () => {
-    console.log(`ta rodando em http://localhost:${port}`);
-  });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ where: { email } });
+  if (!user) {
+    return res.status(401).send("Invalid email or password");
+  }
+  const passwordMatch = bcrypt.compareSync(password, user.password);
+  if (!passwordMatch) {
+    return res.status(401).send("Invalid email or password");
+  }
+  const token = generateToken(user.dataValues);
+  delete user.dataValues.password;
+  res.send({ user, token });
+});
+
+app.get("/users", authenticateToken, async (req, res) => {
+  const users = await User.findAll();
+  res.send(users);
+});
+app.listen(port, () => {
+  console.log(`ta rodando em http://localhost:${port}`);
+});
--- back-end/src/middlewares/authService.js
+++ back-end/src/middlewares/authService.js
@@ -1,29 +1,32 @@
-const jsonwebtoken = require('jsonwebtoken');
+const jsonwebtoken = require("jsonwebtoken");
 
-const secret = 'mysecretkey';
+const secret = process.env.SECRET;
 function generateToken(user) {
   return jsonwebtoken.sign(user, secret, {
-    expiresIn: '1m',
+    expiresIn: "1h",
   });
 }
 
 function verifyToken(token) {
   return jsonwebtoken.verify(token, secret);
-} 
+}
 
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader
-  .split(' ')[1];
-  if (token == null) {
-    return res.sendStatus(401);
-  }
-  verifyToken(token, (err, user) => {
-    if (err) {
-      return res.sendStatus(403);
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token == null) {
+      return res.sendStatus(401);
     }
-    next();
-  });
+    const verify = verifyToken(token);
+    if (verify) {
+      next();
+      return;
+    }
+    return res.sendStatus(401);
+  } catch (error) {
+    res.status(401).send(error);
+  }
 }
 
 module.exports = { generateToken, authenticateToken };
